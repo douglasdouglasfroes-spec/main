3bd64143c9109b38cf2a1e1fcfb90f3a6f452399
Auzirene <auzireneangelicat@gmail.com>
2026-06-25 20:31:28 -0300

    ajuste de metodos e arquivo pom

--- src/test/java/Bancointer/autBancointer.java
+++ src/test/java/Bancointer/autBancointer.java
@@ -1,39 +1,34 @@
 package Bancointer;
 import static org.junit.Assert.assertEquals;
 
-
-
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.WebDriver;
 
 
 public class autBancointer {
 	//istancia do meu driver
 	WebDriver  driver;
 	
-	@BeforeEach
+	@Before
 	public void setUp() throws Exception {
-		// comando para indicar o tipo de navegador e o caminho do driver do navegador
 		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
-		 driver = new ChromeDriver();
+		 driver= new ChromeDriver();
 		 driver.manage().window().maximize();
 		 driver.get("https://inter.co/");
 		
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 		
 	}
 
 	@Test
-	void testaberturadeconta() throws InterruptedException {
+	public void testaberturadeconta() throws InterruptedException {
 		 Thread.sleep(2000);
 		driver.findElement(By.cssSelector("#my-scroll-container > div.sc-1eyyf0l-1.llAUKn.d-flex.align-items-start.align-items-lg-center > div > div > div > div > div > button")).click();   
         Thread.sleep(10000);
